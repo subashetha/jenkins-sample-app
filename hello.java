c270d1318183d742966b37a1d395ed464c6bcf33
SubaShetha <subaidol45@gmail.com>
2026-02-14 15:53:15 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,6 @@
-System.out.println("java")
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins! Build Stage Successful.");
+        System.out.println("Current Date: 20/01/2026");
+    }
+}
